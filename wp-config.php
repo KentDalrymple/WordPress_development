20e197dadbd1258dc88128c3e50085a4f2032264
Kent <30663712+KentDalrymple@users.noreply.github.com>
2018-10-20 06:44:27 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,15 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'dummy_name');
-
+define('DB_NAME', 'd9osm2jllrji9t');
 /** MySQL database username */
-define('DB_USER', 'dummy_user');
-
+define('DB_USER', 'gmpxnqagjamhqv');
 /** MySQL database password */
-define('DB_PASSWORD', 'dummy_password');
-
+define('DB_PASSWORD', 'c96940137c99c53086007f29eae4eb347adfc4dbf16cb78f8a86da1d307835f6');
 /** MySQL hostname */
-define('DB_HOST', 'dummy_host');
+define('DB_HOST', 'ec2-107-20-211-10.compute-1.amazonaws.com:5432');
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
