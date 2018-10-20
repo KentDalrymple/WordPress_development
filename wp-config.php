f967f42875d4fcd05713552b339461223aae1ce6
Kent <30663712+KentDalrymple@users.noreply.github.com>
2018-10-20 07:09:32 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,17 +20,17 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'd9osm2jllrji9t');
+define('DB_NAME', getenv('DB_NAME'));
 /** MySQL database username */
-define('DB_USER', 'gmpxnqagjamhqv');
+define('DB_USER', getenv('DB_USER'));
 /** MySQL database password */
-define('DB_PASSWORD', 'c96940137c99c53086007f29eae4eb347adfc4dbf16cb78f8a86da1d307835f6');
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 /** MySQL hostname */
-define('DB_HOST', 'ec2-107-20-211-10.compute-1.amazonaws.com:5432');
+define('DB_HOST', getenv('DB_HOST'));
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', getenv('DB_CHARSET'));
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define('DB_COLLATE', getenv('DB_COLLATE'));
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -41,16 +41,16 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'xK?0%zG$>9|D0cdOAl05x+vPH&R5xRvj)Pr{sQL>F^j_56sS WsVt@3TbQl{4zx{');
-define('SECURE_AUTH_KEY',  '|<1RFs5)g5rr7`Pt?K`VoTG_@eJ&ylj=fpmu2;vLum8dESCqA*(GyRu0e(BOt9vx');
-define('LOGGED_IN_KEY',    'px{Qt`I8TDm$4{$S<MLb3t{G}hZVr{a12+WWV7D3Y^DI*@po}nhop?rZOhoB9!9S');
-define('NONCE_KEY',        '.QVp4aqE]W)Zi}_[X+)a0G?TqS:~WD]4Jy%q~N0eT O_E-!-mzKJ~86>+N<i0i E');
-define('AUTH_SALT',        'wJRZnS|{u<?Nsi4m83m)=No@Aq`-.N)D*D(`}h5ewnxd&HlZ!wT8;*VUgu$OUZ*/');
-define('SECURE_AUTH_SALT', 'SC0l$UW_!m@^#RJR zStvi$r;mEApk6%94|vFXE9#W~us*qiYiTo^U^x v%`MfFI');
-define('LOGGED_IN_SALT',   'w~GZ(h?ml@|c*#b9K#P<})Sy-zch.CZAB~cDanG/b/]jt$ow[5sZlF(&KT@@sz;+');
-define('NONCE_SALT',       'F==a7@`DtY`jHox^}caoc+)st0_A<w$g)2R/Bmn$R[F3YX$3a)~j<[8*[E,>_n-J');
-define('JWT_AUTH_SECRET_KEY', 'w~GZ(h?ml@|c*#b9K#P<}N0eT O_E-!-mzb3t{G}hZVr{a12+WWWsVt@3TbQl');
-define('JWT_AUTH_CORS_ENABLE', true);
+define('AUTH_KEY',              getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY',       getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY',         getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY',             getenv('NONCE_KEY'));
+define('AUTH_SALT',             getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT',      getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT',        getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT',            getenv('NONCE_SALT'));
+define('AWS_ACCESS_KEY_ID',     getenv('AWS_ACCESS_KEY_ID'));
+define('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
 
 /**#@-*/
 
